0b8380886c309d59fb250297dd50d923e8b9cfa4
Oyaki122 <oc.yaki122@gmail.com>
2020-05-19 21:17:02 +0900

    項目別検索を実装
    Co-authored-by: TanabeTakehiro <takehirojapan5@gmail.com>

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$('#submit').on('click', function () {
+$('#keyword_submit').on('click', function () {
   var keyword = $('#keyword').val()
   console.log(keyword)
 
@@ -9,6 +9,55 @@ $('#submit').on('click', function () {
     data: { keywords: keyword },
     dataType: 'jsonp',
     jsonp: 'callback',
+    success: function (msg, type) {
+      console.log(msg)
+      console.log(type)
+    },
+    error: function (xhr, status) {
+      console.log(status)
+    },
+  })
+})
+
+$('#item_submit').on('click', function () {
+  var category = $('#category').val()
+  var name = $('#name').val()
+  var constant1 = $('#constant1').val()
+  var constant2 = $('#constant2').val()
+  var number = $('#number').val()
+  var place = $('#place').val()
+  var date = $('#date').val()
+  var updatedBy = $('#updatedBy').val()
+
+  category = category !== '' ? category : undefined
+  name = name !== '' ? name : undefined
+  constant1 = constant1 !== '' ? constant1 : undefined
+  constant2 = constant2 !== '' ? constant2 : undefined
+  number = number !== '' ? number : undefined
+  place = place !== '' ? place : undefined
+  date = date !== '' ? new Date(date).toDateString() : undefined
+  updatedBy = updatedBy !== '' ? updatedBy : undefined
+
+  var obj = {
+    category,
+    name,
+    constant1,
+    constant2,
+    number,
+    place,
+    date,
+    updatedBy,
+  }
+
+  var data = JSON.parse(JSON.stringify(obj))
+  console.log(data)
+
+  $.ajax({
+    type: 'GET',
+    url:
+      'https://script.google.com/macros/s/AKfycbx56aJ09eiV_QJ_4IJJAiHuYV5BkVJWGQi2xzRR3QZxz3YbuXM/exec',
+    data: data,
+    dataType: 'jsonp',
     success: function (msg, type) {
       console.log(msg)
       console.log(type)
