a310636d7b38fc22ecb4cfad44a767b8aba1a781
Oyaki122 <oc.yaki122@gmail.com>
2020-06-03 13:32:01 +0900

    ソート、フィルターを実装

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+var list = []
+
+$(function () {
+  $.ajax({
+    type: 'GET',
+    url:
+      'https://script.google.com/macros/s/AKfycbx56aJ09eiV_QJ_4IJJAiHuYV5BkVJWGQi2xzRR3QZxz3YbuXM/exec',
+    data: { getList: 0 },
+    dataType: 'jsonp',
+    jsonp: 'callback',
+    success: function (msg, type) {
+      console.log(msg)
+      console.log(type)
+
+      var categories = msg.category
+      categories.sort()
+      for (var i = 0; i < categories.length; i++) {
+        var option = $('<option></option>')
+          .text(categories[i])
+          .val(categories[i])
+        $('#category').append(option.clone())
+        $('#categoryFilter').append(option.clone())
+      }
+      var places = msg.place
+      places.sort()
+      for (var i = 0; i < places.length; i++) {
+        var option = $('<option></option>').text(places[i]).val(places[i])
+        $('#place').append(option.clone())
+        $('#placeFilter').append(option.clone())
+      }
+    },
+    error: function (xhr, status) {
+      console.log(status)
+    },
+  })
+})
+
 $('#keyword').keypress(function (e) {
   if (e.which == 13) {
     $('#keyword_submit').click()
@@ -10,6 +47,7 @@ $('.searchByItem > h2').on('click', function () {
 })
 
 $('#keyword_submit').on('click', function () {
+  $('#searching').show()
   var keyword = $('#keyword').val()
   console.log(keyword)
 
@@ -20,10 +58,12 @@ $('#keyword_submit').on('click', function () {
     data: { keywords: keyword },
     dataType: 'jsonp',
     jsonp: 'callback',
+
     success: function (msg, type) {
       console.log(msg)
       console.log(type)
       showList(msg)
+      list = msg
     },
     error: function (xhr, status) {
       console.log(status)
@@ -36,6 +76,7 @@ $('.searchItem > input').keypress(function (e) {
   }
 })
 $('#item_submit').on('click', function () {
+  $('#searching').show()
   var category = $('#category').val()
   var name = $('#name').val()
   var constant1 = $('#constant1').val()
@@ -55,14 +96,14 @@ $('#item_submit').on('click', function () {
   updatedBy = updatedBy !== '' ? updatedBy : undefined
 
   var obj = {
-    category,
-    name,
-    constant1,
-    constant2,
-    number,
-    place,
-    date,
-    updatedBy,
+    category: category,
+    name: name,
+    constant1: constant1,
+    constant2: constant2,
+    number: number,
+    place: place,
+    date: date,
+    updatedBy: updatedBy,
   }
 
   var data = JSON.parse(JSON.stringify(obj))
@@ -77,12 +118,105 @@ $('#item_submit').on('click', function () {
     success: function (msg, type) {
       console.log(msg)
       console.log(type)
+      list = msg
       showList(msg)
     },
     error: function (xhr, status) {
       console.log(status)
     },
   })
+})
+
+$('#categoryFilter').change(function (e) {
+  var selected = $('#categoryFilter').val()
+  if (selected == '') {
+    showList(list)
+    return
+  }
+  var filterList = list.filter(function (l) {
+    return l.category === selected
+  })
+  showList(filterList)
+})
+$('#placeFilter').change(function (e) {
+  var selected = $('#placeFilter').val()
+  if (selected == '') {
+    showList(list)
+    return
+  }
+  var filterList = list.filter(function (l) {
+    return l.place === selected
+  })
+  showList(filterList)
+})
+function createSortFunc(name, isUp) {
+  return function (a, b) {
+    var data = []
+    data[0] = a[name]
+    data[1] = b[name]
+    for (var i = 0; i < 2; i++) {
+      if (typeof data[i] !== 'string') continue
+      if (data[i].endsWith('k')) {
+        data[i] = data[i].slice(0, -1)
+        data[i] = Number(data[i]) * 1000
+      } else if (data[i].endsWith('M')) {
+        data[i] = data[i].slice(0, -1)
+        data[i] = Number(data[i]) * 1000000
+      } else if (data[i].endsWith('p')) {
+        data[i] = data[i].slice(0, -1)
+        data[i] = Number(data[i]) / 1000000000000
+      } else if (data[i].endsWith('u')) {
+        data[i] = data[i].slice(0, -1)
+        data[i] = Number(data[i]) / 1000000
+      }
+    }
+
+    if (data[0] < data[1]) return isUp ? -1 : 1
+    if (data[0] > data[1]) return isUp ? 1 : -1
+    return 0
+  }
+}
+
+$('#sort').change(function (e) {
+  var selected = $('#sort').val()
+  if (selected == '') {
+    showList(list)
+    return
+  }
+  var sortList
+  switch (selected) {
+    case 'nameUp':
+      sortList = list.sort(createSortFunc('name', true))
+      break
+    case 'constant1Up':
+      sortList = list.sort(createSortFunc('constant1', true))
+      break
+    case 'constant2Up':
+      sortList = list.sort(createSortFunc('constant2', true))
+      break
+    case 'numberUp':
+      sortList = list.sort(createSortFunc('nubmer', true))
+      break
+    case 'dateUp':
+      sortList = list.sort(createSortFunc('date', true))
+      break
+    case 'nameDown':
+      sortList = list.sort(createSortFunc('name', false))
+      break
+    case 'constant1Down':
+      sortList = list.sort(createSortFunc('constant1', false))
+      break
+    case 'constant2Down':
+      sortList = list.sort(createSortFunc('constant2', false))
+      break
+    case 'numberDown':
+      sortList = list.sort(createSortFunc('number', false))
+      break
+    case 'dateDown':
+      sortList = list.sort(createSortFunc('date', false))
+      break
+  }
+  showList(sortList)
 })
 
 function createDateString(date) {
@@ -94,57 +228,61 @@ function showList(list) {
 
   $('#list').empty()
 
-  for (l of list) {
+  var itemNum = $('<h4></h4>').text('検索結果: ' + list.length + '件')
+  $('#list').append(itemNum)
+
+  for (var i in list) {
     var box = $('<div></div>', {
       class: 'resultBox',
     })
 
     var categoryNameWrap = $('<div></div>').addClass('categoryNameWrap')
-    var category = $('<p></p>').text(l.category).addClass('category')
-    var name = $('<h3></h3>').text(l.name).addClass('name')
+    var category = $('<p></p>').text(list[i].category).addClass('category')
+    var name = $('<h3></h3>').text(list[i].name).addClass('name')
     var number = $('<p></p>')
-      .text(l.number + '個')
+      .text(list[i].number + '個')
       .addClass('number')
     categoryNameWrap.append(category, name, number)
     var valueWrap = $('<div></div>').addClass('valueWrap')
 
     var constantWrap = $('<div></div>').addClass('nameWrap')
     var constant1 = $('<p></p>')
-      .text(l.constant1 != 'not set' ? l.constant1 : '')
+      .text(list[i].constant1 != 'not set' ? list[i].constant1 : '')
       .addClass('constant1')
     var constant2 = $('<p></p>')
-      .text(l.constant2 != 'not set' ? l.constant2 : '')
+      .text(list[i].constant2 != 'not set' ? list[i].constant2 : '')
       .addClass('constant2')
     constantWrap.append(constant1, constant2)
 
     var datePlaceWrap = $('<div></div>').add('datePlaceWrap')
     var date = $('<p></p>')
-      .text(createDateString(new Date(l.date)))
+      .text(createDateString(new Date(list[i].date)))
       .addClass('date')
-    var place = $('<p></p>').text(l.place).addClass('place')
-    var updatedBy = $('<p></p>').text(l.updatedBy).addClass('updatedBy')
+    var place = $('<p></p>').text(list[i].place).addClass('place')
+    var updatedBy = $('<p></p>').text(list[i].updatedBy).addClass('updatedBy')
     datePlaceWrap.append(date, place, updatedBy)
 
     valueWrap.append(categoryNameWrap, constantWrap, datePlaceWrap)
 
     var urlRemarkWrap = $('<div></div>').addClass('urlRemarkWrap')
 
-    var URL =
-      l.url != 'not set'
-        ? $('<p></p>')
-            .append(
-              $('<a></a>', {
-                href: l.url,
-              }).text(l.url),
-            )
-            .addClass('url')
-        : undefined
+    var URL = undefined
+    if (list[i].url != 'not set') {
+      URL = $('<p></p>').append(
+        $('<a></a>', {
+          href: list[i].url,
+          class: 'url',
+        }).text(list[i].url)
+      )
+    }
     var remark = $('<p></p>')
-      .text(l.remark != 'not set' ? l.remark : '')
+      .text(list[i].remark != 'not set' ? list[i].remark : '')
       .addClass('remark')
 
     urlRemarkWrap.append(URL, remark)
     box.append(valueWrap, urlRemarkWrap)
     $('#list').append(box)
   }
+  $('#result').show()
+  $('#searching').hide()
 }
